571afc5a3bfdb256b384717fabe4d3854c7cde96
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2023-03-11 22:12:57 +0300

    Added pinia

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,15 +1,18 @@
 import '@/bootstrap';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import router from '@/router';
 
 import DefaultLayout from '@/layouts/default.vue';
 
 const app = createApp(DefaultLayout);
 app.use(router);
+app.use(createPinia());
 
 // @ts-ignore
 Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+    // @ts-ignore
     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
 });
 
